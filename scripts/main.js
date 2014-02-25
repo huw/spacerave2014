94727c738abac8b966d266f364cac80a6dff8cca
Huw Evans <huw1999@gmail.com>
2014-02-25 18:31:43 +1100

    Basic movement works!

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
-
 	/****** INITIAL DEFINITION *****/
-	$('canvas').attr('width', $(window).innerWidth());	// Resize the canvas
-	$('canvas').attr('height', $(window).innerHeight() - 5);
+	var cWidth = $(window).innerWidth() - 5;
+	var cHeight = $(window).innerHeight() - 5;
+
+	$('canvas').attr('width', cWidth);	// Resize the canvas
+	$('canvas').attr('height', cHeight);
 
 	var canvas = document.getElementById("canvas").getContext("2d");	// Pull our canvas
 
@@ -35,8 +37,8 @@ $(document).ready(function(){
 	/****** DEFINE VARIABLES ******/
 	var ship = {
 		color : "#00BBFF",
-		x     : $(window).innerWidth() / 2,
-		y     : $(window).innerHeight() - 60,
+		x     : cWidth / 2,
+		y     : cHeight - 60,
 		width : 30,
 		height: 30,
 		speed : 5,
@@ -50,18 +52,34 @@ $(document).ready(function(){
 	function update() {
 		if (keydown.left || keydown.a) {
 			ship.x -= ship.speed;
+
+			if (ship.x <= 0) {	// Reset position if we go out of bounds
+				ship.x = 0;
+			}
 		}
 
 		if (keydown.right || keydown.d) {
 			ship.x += ship.speed;
+
+			if (ship.x >= cWidth - ship.width) {
+				ship.x = cWidth - ship.width;
+			}
 		}
 
 		if (keydown.up || keydown.w) {
 			ship.y -= ship.speed;
+
+			if (ship.y <= 0) {
+				ship.y = 0;
+			}
 		} 
 
 		if (keydown.down || keydown.s) {
 			ship.y += ship.speed;
+
+			if (ship.y >= cHeight - ship.height) {
+				ship.y = cHeight - ship.height;
+			}
 		}
 
 		if (keydown.shift) {
@@ -72,7 +90,7 @@ $(document).ready(function(){
 	}
 
 	function draw() {
-		canvas.clearRect(0, 0, $(window).innerWidth(), $(window).innerHeight());
+		canvas.clearRect(0, 0, cWidth, cHeight);
 
 		ship.draw();
 	}
