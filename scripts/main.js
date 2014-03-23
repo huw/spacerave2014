d272f1bab03d977616a508191b6a6f1badb481d7
Huw Evans <huw1999@gmail.com>
2014-03-23 14:50:49 +1100

    Test for a mobile browser before rendering buttons

--- scripts/main.js
+++ scripts/main.js
@@ -283,6 +283,10 @@ $(document).ready(function(){
 	    return 0;
 	}
 
+	/****** JQUERY MOBILE BROWSER DETECTOR *****/
+	/* Courtesy of detectmobilebrowsers.com */
+	(function(a){(jQuery.browser=jQuery.browser||{}).mobile=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
+
 	/****** DEFINE VARIABLES ******/
 	var bullets      = [];	// We have a bunch of arrays.
 	var alienBullets = [];	// These store things like
@@ -324,7 +328,10 @@ $(document).ready(function(){
 	var mousePos;
 	var buttondown = {
 		left: false,
-		right: false
+		right: false,
+		up: false,
+		down: false,
+		fire: false
 	}
 	var mouseDown = false;
 
@@ -540,6 +547,64 @@ $(document).ready(function(){
 		}));
 	}
 
+	var arrow = {
+		left: {
+			x: 40,
+			y: cHeight - 160,
+			width: 40,
+			height: 80
+		},
+		right: {
+			x: 160,
+			y: cHeight - 160,
+			width: 40,
+			height: 80
+		},
+		up: {
+			x: 80,
+			y: cHeight - 200,
+			width: 80,
+			height: 40
+		},
+		down: {
+			x: 80,
+			y: cHeight - 80,
+			width: 80,
+			height: 40
+		},
+
+		colour: "#FFF",
+		alpha: 0.5,
+
+		draw: function() {
+			canvas.globalAlpha = this.alpha;
+			canvas.fillStyle = this.colour;
+
+			canvas.fillRect(this.left.x, this.left.y, this.left.width, this.left.height);
+			canvas.fillRect(this.right.x, this.right.y, this.right.width, this.right.height);
+			canvas.fillRect(this.up.x, this.up.y, this.up.width, this.up.height);
+			canvas.fillRect(this.down.x, this.down.y, this.down.width, this.down.height);
+
+			canvas.globalAlpha = 1;
+		}
+	}
+
+	var button = {
+		x: cWidth - 120,
+		y: cHeight - 120,
+		width: 80,
+		height: 80,
+		colour: "#FFF",
+		alpha: 0.5,
+
+		draw: function() {
+			canvas.globalAlpha = this.alpha;
+			canvas.fillStyle = this.colour;
+			canvas.fillRect(this.x, this.y, this.width, this.height);
+			canvas.globalAlpha = 1;
+		}
+	}
+
 	function collides(a, b) {	// Collision detection. Very simple, works beautifully.
 		return a.x < b.x + b.width && a.x + a.width > b.x && a.y < b.y + b.height && a.y + a.height > b.y;
 	}
@@ -649,20 +714,6 @@ $(document).ready(function(){
 	};
 
 	/****** ADD EVENT LISTENERS OUTSIDE OF INTERVAL FUNCTIONS ******/
-	var leftHalf = {
-		x: 0,
-		y: 0,
-		width: cWidth / 2,
-		height: cHeight
-	}
-
-	var rightHalf = {
-		x: cWidth / 2,
-		y: 0,
-		width: cWidth / 2,
-		height: cHeight
-	}
-
 	function touchXY(e) {
 		if (!e) {
 			var e = event;
@@ -695,17 +746,26 @@ $(document).ready(function(){
 	function update() {
 		wheelOfSpin();
 
-		if (mouseDown) {
-			if (collides(mousePos, leftHalf)) {
-				buttondown.right = false;
+		if (mouseDown) {	// If we're touching the screen
+			if (collides(mousePos, arrow.left)) {
+				buttondown.up = buttondown.right = buttondown.down = false;
 				buttondown.left = true;
-			} else if (collides(mousePos, rightHalf)) {
-				buttondown.left = false;
+			} else if (collides(mousePos, arrow.right)) {
+				buttondown.left = buttondown.up = buttondown.down = false;
 				buttondown.right = true;
+			} else if (collides(mousePos, arrow.up)) {
+				buttondown.left = buttondown.right = buttondown.down = false;
+				buttondown.up = true;
+			} else if (collides(mousePos, arrow.down)) {
+				buttondown.left = buttondown.right = buttondown.up = false;
+				buttondown.down = true;
+			}
+
+			if (collides(mousePos, button)) {
+				buttondown.fire = true;
 			}
 		} else {
-			buttondown.left = false;
-			buttondown.right = false;
+			buttondown.left = buttondown.right = buttondown.up = buttondown.down = buttondown.fire = false;
 		}
 
 		if (ship.state == "alive") {	// You can't move when you're dead
@@ -713,9 +773,9 @@ $(document).ready(function(){
 				ship.x -= ship.speed;
 			} else if (keydown.right || keydown.d || buttondown.right) {
 				ship.x += ship.speed;
-			} else if (keydown.up || keydown.w) {
+			} else if (keydown.up || keydown.w || buttondown.up) {
 				ship.y -= ship.speed;
-			} else if (keydown.down || keydown.s) {
+			} else if (keydown.down || keydown.s || buttondown.down) {
 				ship.y += ship.speed;
 			}	// BTW, this is in a else if because diagonal movement was too fast
 
@@ -727,7 +787,7 @@ $(document).ready(function(){
 				wasdUsed = true;
 			}
 
-			if (keydown.space) {
+			if (keydown.space || buttondown.fire) {
 				spaceUsed = true;
 
 				if (bulletThrottle === false) {	// If we haven't shot recently
@@ -923,6 +983,11 @@ $(document).ready(function(){
 
 		canvas.fillStyle = "#F0F";
 		canvas.fillRect(0, 0, cWidth * (((elapsed + 330) % 330) / 330), 5);
+
+		if (jQuery.browser.mobile) {
+			arrow.draw();
+			button.draw();
+		}
 	}
 
 	function resetGame() {
